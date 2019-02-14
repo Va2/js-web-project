e1349711fdbe60b4269ebd7426eb41bb6e41cb64
Va2 <vadim.brieven@gmail.com>
2019-02-14 12:36:36 +0100

    Add navbar JS (change style on scroll down)

---
+++ src/js/nav.js
@@ -0,0 +1,20 @@
+// When the user scrolls down 350px from the top of the document => change navbar style
+window.onscroll = () => {
+    scrollFct()
+};
+
+let scrollFct = () => {
+    const navbar = document.getElementById("navbar");
+    const navIcon = document.getElementById('nav-icon');
+
+    if (document.body.scrollTop > 350 || document.documentElement.scrollTop > 350) {
+        navbar.classList.add('fixed-top');
+        navbar.classList.add('py-4');
+        navbar.classList.add('shadow-sm');
+        navbar.style.backgroundColor = '#fff';
+        navIcon.classList.remove('d-none');
+    } else {
+        navbar.classList.remove('fixed-top');
+        navIcon.classList.add('d-none');
+    }
+}
